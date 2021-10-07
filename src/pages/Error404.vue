981d873e0f544621db840d8d20d8b66fe1278001
Bart Duisters <bartduisters@bartduisters.com>
2021-10-07 21:46:03 +0200

    feat: custom 404 page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+  <div id="page404">
+    <div id="content-wrapper">
+      <h1>404</h1>
+      <div id="sub-text">Pagina niet gevonden!</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Error404'
-})
+  name: 'Error404',
+});
 </script>
+
+<style lang="scss" scoped>
+#page404 {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: deeppink;
+  color: white;
+}
+
+#content-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#sub-text {
+  font-size: 3em;
+}
+</style>
